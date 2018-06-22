c270f09dbc7456169e14b7343b6e99432bb1632d
stephaniegoff <40501820+stephaniegoff@users.noreply.github.com>
2018-06-22 14:16:53 -0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,15 @@
+def main():
+    myEmptyList = []
+    myListWithStuff = ["thing", 1, "thing", 2]
+
+    print(myEmptyList)
+    print(myListWithStuff)
+
+    myEmpty2DList = [[]]
+    my2DListThatsFulfilled = [[1,2,3],
+                              [4,5,6],
+                              [7,8,9]]
+
+    print(myEmpty2DList)
+    print(my2DListThatsFulfilled[2][1])
+main()
